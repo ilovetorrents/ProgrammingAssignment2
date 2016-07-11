d1dcd52dcc0e168c5c00728b590f480d643f817e
ilovetorrents <zecevic94uros@gmail.com>
2016-07-11 11:55:08 +0200

    added meaningful comments to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,19 @@
-## A couple of functions that simulate 
-## caching (of a matrix inverse)
-
-## Not much to say about this function
-## It is pretty much identical to the one that
-## the professors provided as an example
-
-## create 4 functions and put them in a list
-## in the end return that list
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        # This function simulates object-oriented paradigm by 
+        # encapsulating a matrix (x) and providing with certain functionality for manipulating that matrix.
+        #
+        # Args:
+        #   x: Matrix to be encapsulated.
+        #
+        # Returns:
+        #   A list of functions that manipulate that matrix
+        #       setMatrix(y) - store another matrix, matrix y, overwriting the previous one
+        #       getMatrix - get the stored matrix
+        #       getInverse - get the stored matrix inverse
+        #       setInvers(i) - store a new value for the inverse, value i
+
         inverse <- NULL
         setMatrix <- function(y) {
                 x <<- y
@@ -23,15 +28,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Again, not much going here either except that solve function is used
-## and not mean in order to calculate the inverse
-
-## check if there's a cached value
-## if yes then return that value
-## if not retrieve the matrix, calculate the inverse and cache it
-## return the inverse
-
 cacheSolve <- function(x, ...) {
+
+        # This function is used to calculate a matrix inverse of the CacheMatrix "object".
+        #
+        # It does so in a way intended to speed up the calculation by simulating caching.
+        # It checks to see if the inverse has been stored, if not then calculate it, "cache" it
+        # and return the inverse.
+        #
+        # Args:
+        #   x: list returned by the makeCacheMatrix function
+        #
+        # Returns:
+        #   value of the inverse calcualted for the matrix encapsulated within the CacehMatrix "object"
+
+
         inverse <- x$getInverse()
         if(!is.null(inverse)) {
                 message("getting cached data")
